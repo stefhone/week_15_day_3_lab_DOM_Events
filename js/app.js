23e4baceba69c6bf5337ed4d9d1ceeff0ab4cf52
Stefan Hone <stefan.hone@live.co.uk>
2021-10-14 00:06:47 +0100

    MVP, EXTENSIONS & STYLING

--- js/app.js
+++ js/app.js
@@ -2,36 +2,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const formElement = document.querySelector('#new-item-form');
   formElement.addEventListener('submit', handleFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
 
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  
+
+  // 1 create element which is a function that is going to create our list item
+  const readingListItem = createReadingListItem(event.target);
+  // 2 give new element content - NO CONTENT REQUIRED SO CAN SKIP THIS PART
+  // 3 add to the DOM attach to parent element, unordered list with id 'readling-list'
+  const readingList = document.querySelector('#reading-list');
+  // 4 use DOM method appendChild
+  readingList.appendChild(readingListItem);
+
+  event.target.reset();
  
-  
-  
-  const listItem = document.createElement('li');
-  listItem.classList = 'list-item';
-  listItem.textContent = ``;
-  const resultListItem = document.querySelector('#reading-list');
-  resultListItem.appendChild(listItem);
+}
 
-  const newH1 = document.createElement('h1');
-  newH1.textContent = event.target.title.value; 
-  const newH1Container = document.querySelector('.list-item');
-  newH1Container.appendChild(newH1); 
+const createReadingListItem = function (form) { 
+  const readingListItem = document.createElement('li');
+  readingListItem.classList = 'list-item';
 
-  const newH3 = document.createElement('h3');
-  newH3.textContent = event.target.author.value; 
-  const newH3Container = document.querySelector('.list-item');
-  newH3Container.appendChild(newH3); 
+  const title = document.createElement('h2');
+  title.textContent = form.title.value; 
+  readingListItem.appendChild(title);
 
-  const newP = document.createElement('p');
-  newP.textContent = event.target.category.value; 
-  const newPContainer = document.querySelector('.list-item');
-  newPContainer.appendChild(newP); 
+  const author = document.createElement('h3');
+  author.textContent = form.author.value; 
+  readingListItem.appendChild(author); 
 
-  const formElement = document.querySelector('#new-item-form');
-  formElement.reset();
+  const category = document.createElement('p');
+  category.textContent = form.category.value; 
+  readingListItem.appendChild(category); 
+
+  return readingListItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const readingList = document.querySelector('#reading-list');
+  readingList.innerHTML = '';
 }
